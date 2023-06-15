7be7142029d923882e48639a8ab2140ba963fd39
LeoLares <leolares97@gmail.com>
2023-06-15 17:20:49 -0300

    modifica price items

--- items/src/main/java/com/microservice/items/controllers/Controller.java
+++ items/src/main/java/com/microservice/items/controllers/Controller.java
@@ -11,7 +11,6 @@ import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -26,23 +25,21 @@ public class Controller {
     @Qualifier("itemServiceFeign")
     public ItemService itemService;
 
-
     @GetMapping("/listarItem")
-    public List<Item> listarItem(){
+    public List<Item> listarItem() {
         return itemService.findAll();
     }
 
-    //aplicar circuitbraker con anotaciones
-
+    // aplicar circuitbraker con anotaciones
 
     @io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker(name = "items")
     @GetMapping("/verItem/{id}/cantidad/{cantidad}")
-    public Item verItem(@PathVariable Long id, @PathVariable Integer cantidad){
+    public Item verItem(@PathVariable Long id, @PathVariable Integer cantidad) {
         return itemService.findAllById(id, cantidad);
     }
 
     // metodo alternativo para el circuit braker
-    public  Item metodoAlternativo(Long id, Integer cantidad, Throwable e){
+    public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
         logger.info(e.getMessage());
         Item item = new Item();
         Product product = new Product();
@@ -53,36 +50,37 @@ public class Controller {
         item.setProduct(product);
         return item;
 
-
     }
-    //el metodo alternatico debe tener el siguiente recurso
-    public CompletableFuture <Item> metodoalternativo2 (Long id, Integer cantidad, Throwable e){
+
+    // el metodo alternatico debe tener el siguiente recurso
+    public CompletableFuture<Item> metodoalternativo2(Long id, Integer cantidad, Throwable e) {
         logger.info(e.getMessage());
         Item item = new Item();
         Product product = new Product();
         item.setCantidad(cantidad);
         product.setId(id);
         product.setName("Calculadora");
-        product.setPrice(2000.00);
+        product.setPrice(3000.00);
         item.setProduct(product);
-        return CompletableFuture.supplyAsync(() ->item);
+        return CompletableFuture.supplyAsync(() -> item);
 
     }
+
     @PostMapping("/crear")
     @ResponseStatus(HttpStatus.CREATED)
-    public Product crear(@RequestBody Product product){
+    public Product crear(@RequestBody Product product) {
         return itemService.save(product);
     }
 
     @PutMapping("/update/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Product update(@RequestBody Product product, Long id){
+    public Product update(@RequestBody Product product, Long id) {
         return itemService.update(product, id);
     }
 
     @DeleteMapping("/delete/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void  delete(@PathVariable Long id){
-         itemService.deleteProduct(id);
+    public void delete(@PathVariable Long id) {
+        itemService.deleteProduct(id);
     }
 }
